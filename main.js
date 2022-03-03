15dfbfa0799e031309ca8e724aff955fac15f0e4
ousseynou8 <ousseynoufofana15@gmail.com>
2022-03-03 16:09:21 +0100

    test

---
+++ main.js
@@ -0,0 +1,25 @@
+import './normalize.css';
+import './css/style.css'; 
+const ul = document.querySelector('#navigation ul')
+const burger = document.querySelector('#burger')
+ul.addEventListener("mouseover", e=>{
+    e.preventDefault;
+    ul.style.top =  "7.5%"
+    ul.style.transitionProperty = "height"
+    ul.style.transitionDelay= "2s"
+    ul.style.transitionDuration= "1s"
+    ul.style.transitionTimingFunction= "cubic-bezier(0.215, 0.610, 0.355, 1)"
+})
+
+ul.addEventListener("mouseleave", e=> {
+    e.preventDefault;
+    ul.style.top= "1%"
+})
+
+
+
+
+burger.addEventListener("click",e => {
+    e.preventDefault;
+    ul.classList.toggle('links-active')
+})()
