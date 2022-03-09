27be3c1d59ae7760b204286d4882718aeae0e095
Mahmoud <dooro.mah@gmail.com>
2022-03-09 16:00:00 +0000

    Increase sass

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
 import './normalize.css';
 import './design/style.css'; 
-// const ul = document.querySelector('#navigation ul')
-// const burger = document.querySelector('#burger')
-// ul.addEventListener("mouseover", e=>{
-//     e.preventDefault;
-//     ul.style.top =  "7.5%"
-//     ul.style.transitionProperty = "height"
-//     ul.style.transitionDelay= "2s"
-//     ul.style.transitionDuration= "1s"
-//     ul.style.transitionTimingFunction= "cubic-bezier(0.215, 0.610, 0.355, 1)"
-// })
+const burger = document.getElementById('burger');
 
-// ul.addEventListener("mouseleave", e=> {
-//     e.preventDefault;
-//     ul.style.top= "1%"
-// })
-
-
-
-
-// burger.addEventListener("click",e => {
-//     e.preventDefault;
-//     ul.classList.toggle('links-active')
-// })()
+burger.addEventListener("click", e => {
+    burger.classList.toggle('burgHover')
+    
+})
