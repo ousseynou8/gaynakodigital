13c9f7e3052d4ce7fc9c5cf49caf3de93328071a
Mahmoud <dooro.mah@gmail.com>
2022-03-11 20:19:38 +0000

    Suppression du master

--- main.js
+++
@@ -1,9 +0,0 @@
-import './normalize.css';
-import './design/style.css'; 
-const burger = document.getElementById('burger');
-const menu = document.getElementById('menu');
-
-burger.addEventListener("click", e => {
-    burger.classList.toggle('burgHover')
-    menu.classList.toggle('menuHover')
-})
