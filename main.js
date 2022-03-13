8ca935e4be8857cd6d4d9e412f06cac9c4410516
Mahmoud <dooro.mah@gmail.com>
2022-03-13 21:04:09 +0000

    Master changed

--- main.js
+++ main.js
@@ -2,8 +2,16 @@ import './normalize.css';
 import './design/style.css'; 
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
-
-burger.addEventListener("click", e => {
+const menuList = document.querySelectorAll('#navigation ul li');
+const menuAction = e => {
+    e.preventDefault;
     burger.classList.toggle('burgHover')
     menu.classList.toggle('menuHover')
+}
+// Click sur chaque élement du menu
+menuList.forEach(li => {
+    li.addEventListener("click", menuAction)
 })
+
+// Click sur le burger
+burger.addEventListener("click", menuAction)
