09640485f1a7b1298c1435b86528010603162391
Mahmoud <dooro.mah@gmail.com>
2022-03-13 21:11:53 +0000

    Reverse last commit

--- main.js
+++ main.js
@@ -2,16 +2,8 @@ import './normalize.css';
 import './design/style.css'; 
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
-const menuList = document.querySelectorAll('#navigation ul li');
-const menuAction = e => {
-    e.preventDefault;
+
+burger.addEventListener("click", e => {
     burger.classList.toggle('burgHover')
     menu.classList.toggle('menuHover')
-}
-// Click sur chaque élement du menu
-menuList.forEach(li => {
-    li.addEventListener("click", menuAction)
 })
-
-// Click sur le burger
-burger.addEventListener("click", menuAction)
