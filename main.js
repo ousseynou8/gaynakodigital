65216f1e61bc2ec53b5bebc7bf4c6eef43e0da2b
ousseynou8 <ousseynoufofana15@gmail.com>
2022-03-13 17:40:50 +0100

    13/03

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ import './normalize.css';
 import './design/style.css'; 
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
+const listMenu = document.querySelectorAll("#navigation ul li");
+
+
+listMenu.forEach(li => {
+    li.addEventListener('click', () => {
+        burger.classList.toggle('burgHover')
+        menu.classList.toggle('menuHover')
+    })
+})
+
+
 
 burger.addEventListener("click", e => {
+    
     burger.classList.toggle('burgHover')
     menu.classList.toggle('menuHover')
 })
