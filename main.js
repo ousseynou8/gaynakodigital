cc3453351012e53b55b6e5646020b16e6ff263e0
Mahmoud <dooro.mah@gmail.com>
2022-03-17 16:48:57 +0000

    Actors update

--- main.js
+++ main.js
@@ -3,19 +3,15 @@ import './design/style.css';
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 const listMenu = document.querySelectorAll("#navigation ul li");
-
-
-listMenu.forEach(li => {
-    li.addEventListener('click', () => {
-        burger.classList.toggle('burgHover')
-        menu.classList.toggle('menuHover')
-    })
-})
-
-
-
-burger.addEventListener("click", e => {
-    
+const burgerAction = () => {
     burger.classList.toggle('burgHover')
     menu.classList.toggle('menuHover')
+}
+
+listMenu.forEach(li => {
+    li.addEventListener('click', burgerAction)
 })
+
+
+
+burger.addEventListener('click', burgerAction)
